e81fe41b43f6a74d0c760b329d4793c7324131f3
Will McAllister <willmca32@gmail.com>
2019-08-28 13:23:21 -0400

    randomization working

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -12,43 +12,46 @@ const response = document.querySelector(".answer")
 let counter = 0
 const scoreCount = document.querySelector("h3")
 const startOver = document.querySelector(".reset")
-
-// Timer function 
-
+let i=0
 
 
 // Event Listeners
 
-check.addEventListener("click", function(evt){
+check.addEventListener("click", function(evt)
+{
     evt.preventDefault();
     console.log("button works")
-    if(response.value.toLowerCase()===pokeName.innerText.toLowerCase()){
+    if(response.value.toLowerCase()===pokeName.innerText.toLowerCase())
+    {
         counter += 1
         scoreCount.innerText= ("Score: " + counter) 
+
     } else {
         pokeName.innerText="Sorry, that's not correct."
-        }
+
+    }
+    pokeImg.src=allPokemon[i].imgfull
     pokeName.style.display="block"
     response.value=""
+    setTimeout(function(){ nextButton.click(); }, 3000);
+
+
 
 })
 
 nextButton.addEventListener("click", function(evt){
     evt.preventDefault();
      function click(){
-        clicks += 1
         console.log(clicks)
         response.value=""
     }
     click();
-
-    let i=clicks
+    let random = Math.ceil(Math.random() * 6)
+    i = random
      pokeImg.src=allPokemon[i].img
      pokeName.innerText= allPokemon[i].name
      pokeName.style.display="none"
-     console.log(allPokemon[i],name)
-     
-})
+     })
 
 startOver.addEventListener("click", function(evt){
     evt.preventDefault();
@@ -65,21 +68,28 @@ startOver.addEventListener("click", function(evt){
 // Class (pokemon) creation
 
 class Pokemon {
-    constructor(name,img){
+    constructor(name,img,imgfull){
         this.name=name
         this.img=img
+        this.imgfull=imgfull
     }
 }
 
 // New pokemon
 
-const charmander = new Pokemon('Charmander', 'file:///Users/carlarojas/Documents/General_Assembly/projects/ProjectOne/images/silhouette_charmander.jpg')
-const squirtle = new Pokemon('Squirtle', 'file:///Users/carlarojas/Documents/General_Assembly/projects/ProjectOne/images/silhouette_squirtle.jpg')
-const dratini = new Pokemon('Dratini', 'file:///Users/carlarojas/Documents/General_Assembly/projects/ProjectOne/images/silhouette_dratini.png')
-const venasaur = new Pokemon('Venasaur', 'file:///Users/carlarojas/Documents/General_Assembly/projects/ProjectOne/images/silhouette_venasaur.jpg')
-const ninetails = new Pokemon('Ninetails', 'file:///Users/carlarojas/Documents/General_Assembly/projects/ProjectOne/images/silhouette_ninetales.png')
-const venonat = new Pokemon('Venonat', 'file:///Users/carlarojas/Documents/General_Assembly/projects/ProjectOne/images/silhouette_venonat.jpg')
-const alakazam = new Pokemon('Alakazam', 'file:///Users/carlarojas/Documents/General_Assembly/projects/ProjectOne/images/silhouette_alakazam.png')
+const charmander = new Pokemon('Charmander', 'file:///Users/carlarojas/Documents/General_Assembly/projects/ProjectOne/images/silhouette_charmander.jpg', 'file:///Users/carlarojas/Documents/General_Assembly/projects/ProjectOne/images/charmander.png')
+
+const squirtle = new Pokemon('Squirtle', 'file:///Users/carlarojas/Documents/General_Assembly/projects/ProjectOne/images/silhouette_squirtle.jpg', 'file:///Users/carlarojas/Documents/General_Assembly/projects/ProjectOne/images/squirtle.png')
+
+const dratini = new Pokemon('Dratini', 'file:///Users/carlarojas/Documents/General_Assembly/projects/ProjectOne/images/silhouette_dratini.png', 'file:///Users/carlarojas/Documents/General_Assembly/projects/ProjectOne/images/dratini.png')
+
+const venasaur = new Pokemon('Venasaur', 'file:///Users/carlarojas/Documents/General_Assembly/projects/ProjectOne/images/silhouette_venasaur.jpg', 'file:///Users/carlarojas/Documents/General_Assembly/projects/ProjectOne/images/venasaur.png')
+
+const ninetales = new Pokemon('Ninetales', 'file:///Users/carlarojas/Documents/General_Assembly/projects/ProjectOne/images/silhouette_ninetales.png', 'file:///Users/carlarojas/Documents/General_Assembly/projects/ProjectOne/images/ninetales.png')
+
+const venonat = new Pokemon('Venonat', 'file:///Users/carlarojas/Documents/General_Assembly/projects/ProjectOne/images/silhouette_venonat.jpg', 'file:///Users/carlarojas/Documents/General_Assembly/projects/ProjectOne/images/venonat.png')
+
+const alakazam = new Pokemon('Alakazam', 'file:///Users/carlarojas/Documents/General_Assembly/projects/ProjectOne/images/silhouette_alakazam.png', 'file:///Users/carlarojas/Documents/General_Assembly/projects/ProjectOne/images/alakazam.png')
 //Pokemon Array
 
-const allPokemon=[charmander,squirtle,dratini,venasaur,ninetails,venonat,alakazam]
+const allPokemon=[charmander,squirtle,dratini,venasaur,ninetales,venonat,alakazam]
